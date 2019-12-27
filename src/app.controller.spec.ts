d99b0af3a924c93f0f23b6f178d37f975466edea
mickersicc <mickersicc@gmail.com>
2019-12-27 18:39:55 -0500

    Changed 'user' to 'app' and fixed tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Test 2 successful!"', () => {
+      expect(appController.getTest()).toBe('Test 2 successful!');
     });
   });
 });
